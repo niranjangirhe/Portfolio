0441080f864c177cac90ccf9ff3854b732f2aa5b
niranjangirhe <niranjangirheindia@gmail.com>
2023-01-01 08:39:43 +0530

    removed cursor

--- assets/js/main.js
+++ assets/js/main.js
@@ -518,39 +518,39 @@ else {
     goLight();
 }
 
-(function () {
-    var follower, init, mouseX, mouseY, positionElement, printout, timer;
+// (function () {
+//     var follower, init, mouseX, mouseY, positionElement, printout, timer;
 
-    follower = document.getElementById('follower');
+//     follower = document.getElementById('follower');
 
-    printout = document.getElementById('printout');
+//     printout = document.getElementById('printout');
 
-    mouseX = (event) => {
-        return event.clientX;
-    };
+//     mouseX = (event) => {
+//         return event.clientX;
+//     };
 
-    mouseY = (event) => {
-        return event.clientY;
-    };
+//     mouseY = (event) => {
+//         return event.clientY;
+//     };
 
-    positionElement = (event) => {
-        var mouse;
-        mouse = {
-            x: mouseX(event),
-            y: mouseY(event)
-        };
-        follower.style.top = mouse.y + 'px';
-        return follower.style.left = mouse.x + 'px';
-    };
+//     positionElement = (event) => {
+//         var mouse;
+//         mouse = {
+//             x: mouseX(event),
+//             y: mouseY(event)
+//         };
+//         follower.style.top = mouse.y + 'px';
+//         return follower.style.left = mouse.x + 'px';
+//     };
 
-    timer = false;
+//     timer = false;
 
-    window.onmousemove = init = (event) => {
-        var _event;
-        _event = event;
-        return timer = setTimeout(() => {
-            return positionElement(_event);
-        }, 1);
-    };
+//     window.onmousemove = init = (event) => {
+//         var _event;
+//         _event = event;
+//         return timer = setTimeout(() => {
+//             return positionElement(_event);
+//         }, 1);
+//     };
 
-}).call(this);
+// }).call(this);
